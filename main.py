b01fec94076bdbd50f16c7473b303d89ffad1ff3
Rohit <rohitmandal0804@gmail.com>
2026-09-10 17:56:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-  return 'Hello World from GitHub on Local host! 😎'
+  return 'Hello World from the gangster of Basirhat Goat Rohit Mandal, My english is englishing , to dont dare to toucg  😎'
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8080, debug=True)
